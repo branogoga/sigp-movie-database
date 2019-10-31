72b901b813b698a659854d422cb4b99337854ac5
Branislav Goga <brano.goga@centrum.sk>
2019-10-31 17:18:34 +0100

    Add content definition components

--- src/Application.tsx
+++ src/Application.tsx
@@ -3,18 +3,59 @@ import * as ReactDOM from "react-dom";
 
 import { LikeButton } from "./Component";
 
-import NavBar from "../node_modules/react-bootstrap/Navbar";
+import Nav from "../node_modules/react-bootstrap/Nav";
+import Navbar from "../node_modules/react-bootstrap/Navbar";
 
 export interface IMenuProps {
 }
 
 const Menu: React.StatelessComponent<IMenuProps> = (props: IMenuProps) => {
     return (
-        <NavBar bg="dark" variant="dark">
-            <NavBar.Brand href="#home">
+        <Navbar bg="dark" variant="dark" fixed="top" collapseOnSelect expand="lg">
+            <Navbar.Brand href="#home">
                 {"Movie Database"}
-            </NavBar.Brand>
-        </NavBar>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#search">Search</Nav.Link>
+                    <Nav.Link href="#favorites">Favorites</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+};
+
+interface IFavoriteMoviesProps {
+}
+
+const FavoriteMovies: React.StatelessComponent<IFavoriteMoviesProps> = (props: IFavoriteMoviesProps) => {
+    return (
+        <div className="mt-5">
+            <h1>Favorite movies</h1>
+        </div>
+    );
+};
+
+interface ISearchMoviesProps {
+}
+
+const SearchMovies: React.StatelessComponent<ISearchMoviesProps> = (props: ISearchMoviesProps) => {
+    return (
+        <div className="mt-5">
+            <h1>Search movies</h1>
+        </div>
+    );
+};
+
+interface IMovieDetailsProps {
+}
+
+const MovieDetails: React.StatelessComponent<IMovieDetailsProps> = (props: IMovieDetailsProps) => {
+    return (
+        <div className="mt-5">
+            <h1>Movie details</h1>
+        </div>
     );
 };
 
@@ -35,10 +76,9 @@ export class Application extends React.Component<IApplicationProps, IApplication
     return (
         <React.Fragment>
             <Menu />
-            <LikeButton
-                caption={this.props.caption}
-                finalText={this.props.finalText}
-                />
+            <div className="my-5">
+                <SearchMovies />
+            </div>
         </React.Fragment>
     );
   }
